27c55854b726f6153c9cee15cb1825195782c211
Carlooo <merin.johncarlo06@gmail.com>
2025-06-01 02:05:14 +0800

    refactor(component): remove template tags causing error not show dropdown body

--- resources/views/components/navs/dropdown.blade.php
+++ resources/views/components/navs/dropdown.blade.php
@@ -43,17 +43,15 @@
         {{ $trigger }}
     </div>
 
-    <template>
-        <div x-show="open" x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100"
-            x-transition:leave-end="opacity-0 scale-95"
-            class="absolute z-50 w-32 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;" @click="open = false">
-            <div
-                class="rounded-md ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 dark:divide-gray-600 {{ $contentClasses }}">
-                {{ $content }}
-            </div>
+    <div x-show="open" x-transition:enter="transition ease-out duration-200"
+        x-transition:enter-start="opacity-0 scale-95" x-transition:enter-end="opacity-100 scale-100"
+        x-transition:leave="transition ease-in duration-75" x-transition:leave-start="opacity-100 scale-100"
+        x-transition:leave-end="opacity-0 scale-95"
+        class="absolute z-50 w-32 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+        style="display: none;" @click="open = false">
+        <div
+            class="rounded-md ring-1 ring-black ring-opacity-5 divide-y divide-gray-200 dark:divide-gray-600 {{ $contentClasses }}">
+            {{ $content }}
         </div>
-    </template>
+    </div>
 </div>
