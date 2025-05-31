18dcd7834bceeb57dd1488e4cfbafee9a97621b5
Carlooo <merin.johncarlo06@gmail.com>
2025-06-01 02:02:34 +0800

    refactor(task): include section and data in main task for table and list view

--- resources/views/contents/tasks.blade.php
+++ resources/views/contents/tasks.blade.php
@@ -1,11 +1,96 @@
 <x-app-layout>
     @include('contents.tasks.header')
-    <main class="mt-5 pb-8">
+    <main x-data="taskTable(), taskList()" x-init="loadTasks()" x-effect="$store.taskEvents.reload && loadTasks()" class="mt-5 pb-8">
         <div class="bg-gray-50 dark:bg-gray-800 p-4 md:p-5 rounded-md border border-gray-200 dark:border-gray-600">
-            @include('contents.tasks.table-view')
-            @include('contents.tasks.list-view')
+            <section id="table-view" style="display:none;">
+                @include('contents.tasks.table-view-partials.filtering-view')
+                @include('contents.tasks.table-view')
+                @include('contents.tasks.table-view-partials.pagination-view')
+            </section>
+
+            <section id="list-view" style="display:none;">
+                @include('contents.tasks.list-view-partials.filtering-view')
+                @include('contents.tasks.list-view')
+            </section>
+
         </div>
+
+        <x-modal title="Update Task" name="update-tasks-modal">
+            <div x-data="taskForm('update', selectedTask)">
+                <form @submit.prevent="submit" class="space-y-4">
+                    <!-- Title -->
+                    <div>
+                        <x-forms.input-label for="title" value="{{ __('Title') }}" />
+                        <x-forms.text-input color="blue" type="text" name="title" id="title"
+                            fieldName="title" x-model="selectedTask.title" placeholder="Title..."
+                            extraClass="focus:border-blue-500"></x-forms.text-input>
+                        <x-forms.input-error :messages="$errors->get('title')" x-text="errors?.title?.[0]" class="mt-2" />
+                    </div>
+
+                    <div class="flex items-center gap-4 w-full">
+                        <!-- Priority -->
+                        <div class="w-full">
+                            <x-forms.input-label for="priority" value="{{ __('Priority') }}" />
+                            <x-forms.select-input color="blue" name="priority" id="priority" fieldName="priority"
+                                x-model="selectedTask.priority" extraClass="focus:border-blue-500">
+                                <option value="">Priority</option>
+                                <option value="low">Low Priority</option>
+                                <option value="medium">Medium Priority</option>
+                                <option value="high">High Priority</option>
+                            </x-forms.select-input>
+                            <x-forms.input-error :messages="$errors->get('priority')" x-text="errors?.priority?.[0]" class="mt-2" />
+                        </div>
+
+                        <!-- Due Date -->
+                        <div class="w-full">
+                            <x-forms.input-label for="due_date" value="{{ __('Due Date (optional)') }}" />
+                            <x-forms.text-input color="blue" type="date" name="due_date" fieldName="due_date"
+                                x-model="selectedTask.due_date" id="due_date"
+                                extraClass="focus:border-blue-500"></x-forms.text-input>
+                            <x-forms.input-error :messages="$errors->get('due_date')" x-text="errors?.due_date?.[0]" class="mt-2" />
+                        </div>
+                    </div>
+
+                    <!-- Details -->
+                    <div>
+                        <x-forms.input-label for="details" value="{{ __('Details') }}" />
+                        <x-forms.text-area color="blue" name="details" id="details" fieldName="details"
+                            rows="4" x-model="selectedTask.details" placeholder="Details..."
+                            extraClass="focus:border-blue-500"></x-forms.text-area>
+                        <x-forms.input-error :messages="$errors->get('details')" x-text="errors?.details?.[0]" class="mt-2" />
+                    </div>
+
+
+
+                    <!-- Actions -->
+                    <div class="flex justify-end space-x-3 pt-4">
+                        <x-forms.button color="gray" type="button"
+                            @click="$dispatch('close')">Close</x-forms.button>
+                        <x-forms.button color="blue" type="submit" name="update-tasks">Update Task</x-forms.button>
+                    </div>
+                </form>
+            </div>
+        </x-modal>
+
+        <x-modal :header="false" maxWidth="lg" title="Delete Task" name="delete-tasks-modal">
+            <div x-data="taskForm('delete', selectedTask)">
+                <form @submit.prevent="submit" class="space-y-4 ">
+                    <div class="flex flex-col items-center justify-center space-y-5">
+                        <svg class='text-red-500 dark:text-red-500 w-14 h-14' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                        <p class="mb-5 text-center text-gray-500 dark:text-gray-300">Are you sure you want to delete this item?</p>
+                    </div>
+                    <div class="flex justify-center items-center space-x-4">
+                        <x-forms.button color="gray" type="button"
+                            @click="$dispatch('close')">Cancel</x-forms.button>
+                        <x-forms.button color="red" type="submit" name="delete-tasks">Yes, I'm sure</x-forms.button>
+                    </div>
+                </form>
+            </div>
+        </x-modal>
     </main>
+    
     <script>
         document.addEventListener('alpine:init', () => {
             Alpine.store('taskEvents', {
@@ -151,14 +236,18 @@
         function taskList() {
             return {
                 tasks: [],
+                selectedTask: {},
+                loading: false,
                 search: '',
+                status: '',
                 priority: '',
-                loading: true,
+                loading: false,
 
                 loadTasks() {
 
                     const params = new URLSearchParams({
                         search: this.search,
+                        status: this.status,
                         priority: this.priority,
                     });
 
