218b0cbeb3d96deefa115143bfd21eb622564580
MewDev <merin.johncarlo.prof@gmail.com>
2025-06-11 15:54:42 +0800

    feat(seeder): create different roles using seeder

---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Spatie\Permission\Models\Role;
+use Illuminate\Database\Seeder;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $collaboratorRole = Role::firstOrCreate(['name' => 'collaborator']);
+        $viewer = Role::firstOrCreate(['name' => 'viewer']);
+    }
+}
