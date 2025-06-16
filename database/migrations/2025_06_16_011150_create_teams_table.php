01a758ca28ec5f5207575c244ec80f9c729c6b74
Carlooo <merin.johncarlo06@gmail.com>
2025-06-16 20:55:58 +0800

    refactor(team): change the color icon into team picture

--- database/migrations/2025_06_16_011150_create_teams_table.php
+++ database/migrations/2025_06_16_011150_create_teams_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('color');
             $table->string('picture');
             $table->text('description')->nullable();
             $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
