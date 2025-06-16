54f51864cb6306a4bb4d4e30b9315e5548cca0bf
MewDev <merin.johncarlo.prof@gmail.com>
2025-06-16 08:25:09 +0800

    refactor(sidebar): add location in tooltip to show different sidepart and add tooltip to the section in every sidebar

--- resources/views/components/templates/tooltip.blade.php
+++ resources/views/components/templates/tooltip.blade.php
@@ -1,6 +1,17 @@
-<!-- resources/views/components/tooltip.blade.php -->
-<div x-data="{ show: false }" @mouseenter="show = true" @mouseleave="show = false" class="relative inline-block">
+@props([
+    'location' => 'top', // default to top
+])
 
+@php
+    $positions = [
+        'top' => 'bottom-full left-1/2 -translate-x-1/2 mb-2',
+        'bottom' => 'top-full left-1/2 -translate-x-1/2 mt-2',
+        'left' => 'right-full top-1/2 -translate-y-1/2 mr-2',
+        'right' => 'left-full top-1/2 -translate-y-1/2 ml-2',
+    ];
+@endphp
+
+<div x-data="{ show: false }" @mouseenter="show = true" @mouseleave="show = false" class="relative inline-block">
     <!-- Trigger -->
     <div>
         {{ $trigger }}
@@ -11,14 +22,8 @@
         x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
         x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 translate-y-0"
         x-transition:leave-end="opacity-0 translate-y-1"
-        class="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 z-50 px-5 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg "
-        style="white-space: nowrap; display: none;">
-
+        class="absolute z-50 px-5 py-2 text-sm font-semibold text-gray-900 bg-gray-50 border border-gray-200 rounded-lg shadow-md {{ $positions[$location] ?? $positions['top'] }}"
+        style="white-space: nowrap; display: none">
         {{ $slot }}
-
-        <!-- Arrow -->
-        <div
-            class="tooltip-bottom shadow relative before:content-[''] before:absolute before:left-1/2 before:bottom-0 before:-translate-x-1/2 before:translate-y-[calc(100%+5px)] before:border-[15px] before:border-transparent before:border-t-white before:border-b-0">
-        </div>
     </div>
 </div>
