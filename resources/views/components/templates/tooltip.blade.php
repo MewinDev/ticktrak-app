2b14364835e1e0b63dd9dbf9a33f85663c91d04c
Carlooo <merin.johncarlo06@gmail.com>
2025-06-16 18:06:16 +0800

    fix(tooltip): fix the error for transition whe nhover a tooltip

--- resources/views/components/templates/tooltip.blade.php
+++ resources/views/components/templates/tooltip.blade.php
@@ -19,9 +19,9 @@
 
     <!-- Tooltip Content -->
     <div x-show="show" x-transition:enter="transition ease-out duration-200"
-        x-transition:enter-start="opacity-0 translate-y-1" x-transition:enter-end="opacity-100 translate-y-0"
-        x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100 translate-y-0"
-        x-transition:leave-end="opacity-0 translate-y-1"
+        x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
+        x-transition:leave="transition ease-in duration-150" x-transition:leave-start="opacity-100"
+        x-transition:leave-end="opacity-0"
         class="absolute z-50 px-5 py-2 text-sm font-semibold text-gray-900 bg-gray-50 border border-gray-200 rounded-lg shadow-md {{ $positions[$location] ?? $positions['top'] }}"
         style="white-space: nowrap; display: none">
         {{ $slot }}
