24f764507f39ac812befa60261a197829bc2b65f
MewDev <merin.johncarlo.prof@gmail.com>
2025-06-19 10:34:33 +0800

    add relationship of task from team

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\SubTask;
+use App\Models\Teams\Team;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -11,7 +12,7 @@ class Task extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'details', 'priority', 'status', 'due_date'];
+    protected $fillable = ['title', 'details', 'priority', 'status', 'due_date', 'team_id'];
 
     /**
      * Get all of the subTasks for the Task
@@ -21,5 +22,15 @@ class Task extends Model
     public function subTasks(): HasMany
     {
         return $this->hasMany(SubTask::class);
+    }
+
+    /**
+     * Get the team that owns the Task
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function team(): BelongsTo
+    {
+        return $this->belongsTo(Team::class);
     }
 }
