5fa8d83265f21f75f06369b256e3e0b850302a2f
MewDev <merin.johncarlo.prof@gmail.com>
2025-06-20 09:02:06 +0800

    feat(model): add team relationship and change the user_id to owner_id for correct naming

--- app/Models/Teams/Team.php
+++ app/Models/Teams/Team.php
@@ -2,23 +2,54 @@
 
 namespace App\Models\Teams;
 
+use App\Models\Teams\TeamMember;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 
 class Team extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['team_name', 'team_code', 'visibility', 'user_id'];
+    protected $fillable = ['team_name', 'team_code', 'visibility', 'owner_id'];
+
+    /**
+     * Get the user that owns the Team
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 
     /**
      * The user that belong to the Team
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function user(): BelongsToMany
+    public function members(): BelongsToMany
     {
         return $this->belongsToMany(User::class, 'team_members')->withPivot('role')->withTimestamps();
+    }
+
+    /**
+     * Get all of the teamMembers for the Team
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function teamMembers(): HasMany
+    {
+        return $this->hasMany(TeamMember::class);
+    }
+
+    /**
+     * Get all of the joinRequests for the Team
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function joinRequests(): HasMany
+    {
+        return $this->hasMany(TeamJoinRequest::class);
     }
 }
