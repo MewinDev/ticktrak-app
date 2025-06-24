de399b5778dbeb29250dff2bb31bd63d83ef40f3
MewDev <merin.johncarlo.prof@gmail.com>
2025-06-24 09:32:06 +0800

    feat(controller): add team controller function to add view update and delete team details

--- app/Http/Controllers/Api/TeamApiController.php
+++ app/Http/Controllers/Api/TeamApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TeamRequest;
 use App\Models\Teams\Team;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class TeamApiController extends Controller
@@ -13,12 +14,42 @@ class TeamApiController extends Controller
         return auth()->user()->joinedTeams()->with('owner')->get();
     }
 
-    public function store(TeamRequest $request) {
-        $team = Team::create($request->validated());
+    public function show($team_name)
+    {
+        $team = Team::where('team_name', $team_name)->with('owner')->firstOrFail();
+        return response()->json($team);
+    }
 
+    public function store(TeamRequest $request) : JsonResponse
+    {
+        $team = Team::create($request->validated());
+ 
         return response()->json([
             'message' => 'Task Created Successfully',
             'task' => $task,
         ], 201);
+    }
+
+    public function updateVisibility(Request $request, Team $team) :JsonResponse
+    {
+        $validated = $request->validate([
+            'visibility' => ['required', 'string']
+        ]);
+
+        $team->visibility = $validated['visibility'];
+        $team->save();
+
+        return response()->json([
+            'message' => 'Update Visibility Successfully',
+        ], 200);
+    }
+
+    public function destroy(Team $team)
+    {
+        $team->delete();
+
+        return response()->json([
+            'message' => 'Team Deleted Successfully',
+        ], 200);
     }
 }
